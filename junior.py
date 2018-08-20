708b9cd7e91857f6bf4d94752e80fa26a6fa60b7
gustavodelima <ghtp25@gmail.com>
2018-08-19 23:47:14 -0300

    update

--- junior.py
+++ junior.py
@@ -3,6 +3,11 @@
 Nascimento = "19 de agosto de 2018"
 Pai_do_Junior = "Gustavo Soares de Lima"
 
-print("Olá, meu nome é Junior")
+class Omega:
+    def Iniciar():
+        print("Olá, meu nome é Junior")
+    def Desligar():
+        print("Vou desligar, tchau!")
 
-print("Vou desligar, tchau!")
+Omega.Iniciar()
+Omega.Desligar()
